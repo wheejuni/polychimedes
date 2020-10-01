e3521e9609cb25b3cadc3630cad00dc7c7872214
wheejuni <wheejuni@gmail.com>
2020-10-01 18:05:52 +0900

    async nonblocking 고루틴으로 수정

--- src/golang/main/main.go
+++ src/golang/main/main.go
@@ -6,9 +6,13 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"sync"
 )
 
-func calculate(size int, dots int, threadFlag int, ratio chan <- float32) {
+func calculate(size int, dots int, threadId int, ratio chan <- float32, group *sync.WaitGroup) {
+	defer group.Done()
+
+	fmt.Printf("thread %d start\n", threadId)
 	inCount := 0
 
 	for dot := 0; dot < dots; dot++ {
@@ -21,28 +25,31 @@ func calculate(size int, dots int, threadFlag int, ratio chan <- float32) {
 			inCount++
 		}
 	}
-	fmt.Printf("thread %d finished\n", threadFlag)
 	ratio <- float32(inCount) / float32(dots)
 }
 
+
 func main() {
-	ratio := make(chan float32)
+	ratio := make(chan float32, 4)
+	wg := &sync.WaitGroup{}
 
-	sum := float32(0)
-
+	wg.Add(4)
 	squareSize, _ := strconv.Atoi(os.Args[2])
 	dotsCount, _ := strconv.Atoi(os.Args[1])
 
 	for i := 0; i < 4; i++ {
-		go calculate(squareSize / 2, dotsCount, i, ratio)
-		select {
-		case receivedRatio := <-ratio:
-			sum += receivedRatio
-
-		default:
-			fmt.Printf("no value received")
-		}
+		go calculate(squareSize / 2, dotsCount, i, ratio, wg)
 	}
 
-	fmt.Printf("%f", sum)
+	wg.Wait()
+	close(ratio)
+
+	result := float32(0)
+
+	for value := range ratio {
+		result += value
+	}
+
+
+	fmt.Printf("result: %f", result)
 }
